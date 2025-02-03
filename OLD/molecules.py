38b616643dd883d4ffd6d77138e4c05f37a28573
Max <max10741412@gmail.com>
2025-02-03 15:41:57 -0700

    MM 0.1

--- OLD/molecules.py
+++
@@ -1,5 +0,0 @@
-import toolset as mm
-#Molecule Format is [ [atom,x,y,z], [atom,x,y,z], [atom,x,y,z] ]
-#Model Format is [ [molecule1], [molecule2] ]
-
-H2O = [["O",-0.04422,0.14218,0],["H",0.67928,0.78678,0],["H",-0.84449,0.68855,0]]
